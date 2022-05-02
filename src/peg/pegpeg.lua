48e8d7b0b9a16408ee117df2ca0e5ee2ebdedd94
Sam Atman <atmanistan@gmail.com>
2022-05-02 18:06:03 -0400

    clean up pegpeg.orb

--- src/peg/pegpeg.lua
+++ src/peg/pegpeg.lua
@@ -5,60 +5,56 @@
 
 
 local pegpeg = [[
-rules  <-  _ rule+ (-1 / Error)
+     rules  ←  _ rule+ (-1 / Error)
 
-rule  <-  lhs rhs
+      rule  ←  lhs rhs
 
-lhs  <-  (suppressed / rule-name) _ into _
-rhs  <-  form _
+       lhs  ←  (suppressed / rule-name) _ into _
+       rhs  ←  form _
 
+suppressed  ←  "`" rule-name "`"
+ rule-name  ←  symbol
+    `into`  ←  ":=" / "←" / "←" / "="
 
-suppressed  <-  "`" rule-name "`"
- rule-name  <-  symbol
-    `into`  <-  ":=" / "<-" / "←" / "="
+    `form`  ←  element _ elements*
+ `element`  ←  !lhs (simple / compound)
+`elements`  ←  (choice / cat) _
 
-    `form`  <-  element _ elements*
- `element`  <-  !lhs (simple / compound)
-`elements`  <-  (choice / cat) _
+    choice  ←  "/" _ form
+       cat  ←  _ form
 
-choice  <- "/" _ form
-   cat  <- _ form
+   `simple` ←  repeated
+            /  matched
+            /  prefixed
+            /  suffixed
+            /  name
+            /  number
+ `compound` ← group / enclosed
 
-`compound` <- group / enclosed
-`simple` <-  repeated
-         /  matched
-         /  prefixed
-         /  suffixed
-         /  name
-         /  number
+    `group` ← "(" _ form _ ")"
+ `enclosed` ←  literal / set / range
 
-`group` <- "(" _ form _ ")"
-`enclosed` <-  literal / set / range
+`repeated`  ←  allow-repeat _ "%" slice
+ `matched`  ←  allow-repeat _ match-suffix
+`prefixed`  ←  not / and
+`suffixed`  ←  zero-or-more / one-or-more / optional
+      name  ←  symbol
+    number  ←  EOS / integer
 
-`repeated`  <-  allow-repeat _ "%" slice
- `matched`  <-  allow-repeat _ match-suffix
-`prefixed`  <-  not / and
-`suffixed`  <-  zero-or-more / one-or-more / optional
-      name  <-  symbol
-    number  <-  EOS / integer
+        literal  ←  single-string / double-string
+            set  ←  "{" (!("}" / "\n") codepoint)* "}"
+          range  ←  "[" range-start "-" range-end "]"
 
-       literal  <-  single-string / double-string
-           set  <-  "{" (!("}" / "\n") 1)* "}"
-         range  <-  "[" range-start "-" range-end "]"
+`single-string`  ←  "'" ("\\" "'" / "\\" utf8 / (!"'" !"\n" utf8))* "'"
+`double-string`  ←  '"' ('\\' '"' / "\\" utf8 / (!'"' !"\n" utf8))* '"'
 
-`single-string`  ←  "'" ("\\" "'" / "\\" 1 / (!"'" !"\n" 1))* "'"
-`double-string`  ←  '"' ('\\' '"' / "\\" 1 / (!'"' !"\n" 1))* '"'
-
-range-start  <-  codepoint
-  range-end  <-  codepoint
-  codepoint  ←  [\x00-\x7f]
-             /  [\xc2-\xdf] [\x80-\xbf]
-             /  [\xe0-\xef] [\x80-\xbf] [\x80-\xbf]
-             /  [\xf0-\xf4] [\x80-\xbf] [\x80-\xbf] [\x80-\xbf]
+range-start  ←  codepoint
+  range-end  ←  codepoint
+  codepoint  ←   utf8
 
 
-       slice  <-  integer-range / integer
-match-suffix  <-  "@" ; whitespace is not allowed. should it be?
+       slice  ←  integer-range / integer
+match-suffix  ←  "@" ; whitespace is not allowed. should it be?
                   ( reference
                   / back-refer
                   / eq-refer
@@ -67,38 +63,43 @@ match-suffix  <-  "@" ; whitespace is not allowed. should it be?
                   / lte-refer
                   / lt-refer )
 
-         not  <-  "!" _ allow-prefix
-         and  <-  "&" _ allow-prefix
-zero-or-more  <-  allow-suffix _ "*"
- one-or-more  <-  allow-suffix _ "+"
-    optional  <-  allow-suffix _ "?"
+           not  ←  "!" _ allow-prefix
+           and  ←  "&" _ allow-prefix
+  zero-or-more  ←  allow-suffix _ "*"
+   one-or-more  ←  allow-suffix _ "+"
+      optional  ←  allow-suffix _ "?"
 
-`allow-repeat`  <-  prefixed / suffixed / jawn
-`allow-prefix`  <-  suffixed / jawn
-`allow-suffix`  <-  prefixed / jawn
-        `jawn`  <-  compound / name / number
+`allow-repeat`  ←  prefixed / suffixed / jawn
+`allow-prefix`  ←  suffixed / jawn
+`allow-suffix`  ←  prefixed / jawn
+        `jawn`  ←  compound / name / number
 
-`symbol`  <-  (([A-Z] / [a-z]) ([A-Z]/[a-z] / {-_})+) / "_"
+      `symbol`  ←  (([A-Z] / [a-z]) ([A-Z]/[a-z] / {-_})+) / "_"
 
-          EOS  <-  "-1"
-integer-range  <-  integer ".." integer
-      integer  <-  [0-9]+
+           EOS  ←  "-1"
+ integer-range  ←  integer ".." integer
+       integer  ←  [0-9]+
 
-back-refer  <-  "(" reference ")"
-eq-refer  <-  "(#" reference ")"
-gte-refer  <-  "(>=" reference ")"
-gt-refer  <-  "(>" reference ")"
-lte-refer  <-  "(<=" reference ")"
-lt-refer  <-  "(<" reference ")"
-reference  <-  symbol
-
-
-      `_`  <-  (comment / dent / { \t\r})*
-`comment`  <-  ";" (!"\n" 1)
-   `dent`  <-  "\n" { \t}*
+    back-refer  ←  "(" reference ")"
+      eq-refer  ←  "(#" reference ")"
+     gte-refer  ←  "(>=" reference ")"
+      gt-refer  ←  "(>" reference ")"
+     lte-refer  ←  "(<=" reference ")"
+      lt-refer  ←  "(<" reference ")"
+     reference  ←  symbol
 
 
-Error  <-  1+
+      `_`  ←  (comment / dent / { \t\r})*
+`comment`  ←  ";" (!"\n" utf8)
+   `dent`  ←  "\n" { \t}*
+
+
+    `utf8`  ←  [\x00-\x7f]
+            /  [\xc2-\xdf] [\x80-\xbf]
+            /  [\xe0-\xef] [\x80-\xbf] [\x80-\xbf]
+            /  [\xf0-\xf4] [\x80-\xbf] [\x80-\xbf] [\x80-\xbf]
+
+     Error  ←  1+
 ]]
 
 
