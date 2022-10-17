9b649cbfde6f07049cd2c94933f2f1a4fee4a1b9
Sam Atman <atmanistan@gmail.com>
2022-10-17 12:27:31 -0400

    Rename 'lisp' to 'elden'
    Starting to set up some basic sessions to exercise the methods

--- src/spec/node.lua
+++ src/spec/node.lua
@@ -23,19 +23,19 @@ local elden = use "cluster:library" ()
 
 
 local elden_peh = [[
-lisp = _ ((atom)+ / list)
-list = pel _ (atom / list)* per _
-atom = _(number / symbol)_
-symbol = _(alpha / other) (alpha / digit / other)*_
-number = float / integer
-`integer` = [0-9]+
-`float` = [0-9]+ "." [0-9]+ ; expand
-`pel` = '('
-`per` = ')'
-`alpha` = [A-Z]/[a-z]
-`digit` = [0-9]
-`other` = {-_-=+!@#$%^&*:/?.\\~}
-  `_`     = { \t\r\n,}*
+    elden  ←  _ ((atom)+ / list)
+     list  ←  pel _ (atom / list)* per _
+     atom  ←  _(number / symbol)_
+   symbol  ←  _(alpha / other) (alpha / digit / other)*_
+   number  ←  float / integer
+`integer`  ←  [0-9]+
+  `float`  ←  [0-9]+ "." [0-9]+ ; expand
+    `pel`  ←  '('
+    `per`  ←  ')'
+  `alpha`  ←  [A-Z]/[a-z]
+  `digit`  ←  [0-9]
+  `other`  ←  {-_-=+!@#$%^&*:/?.\\~}
+      `_`  ←  { \t\r\n,}*
 ]]
 
 
